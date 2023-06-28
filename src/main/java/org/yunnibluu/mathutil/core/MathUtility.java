84898b20b46f8e274223e2021fe42755380dec79
Mbn09 <ngocnguyen.150903@gmail.com>
2023-06-28 11:34:30 +0700

    Add screenshots n update source using recursion to see green 4ever

--- src/main/java/org/yunnibluu/mathutil/core/MathUtility.java
+++ src/main/java/org/yunnibluu/mathutil/core/MathUtility.java
@@ -10,6 +10,20 @@ package org.yunnibluu.mathutil.core;
  */
 public class MathUtility {
     public static final double PI = 3.14;
+//    public static long getFactorial(int n){
+//        if(n < 0 || n > 20)
+//            throw new IllegalArgumentException("Invalid n. n must be between 0..20"); 
+//        
+//        if(n == 0 || n == 1)
+//            return 1;
+//        
+//        long product = 1;
+//        for (int i = 2; i <= n; i++) {
+//            product *=i;
+//        }
+//        return product;
+//    }
+    
     public static long getFactorial(int n){
         if(n < 0 || n > 20)
             throw new IllegalArgumentException("Invalid n. n must be between 0..20"); 
@@ -17,10 +31,7 @@ public class MathUtility {
         if(n == 0 || n == 1)
             return 1;
         
-        long product = 1;
-        for (int i = 2; i <= n; i++) {
-            product *=i;
-        }
-        return product;
+        return n*getFactorial(n-1);
     }
 }
+//  5! = 1.2.3.4.5 = 1.2.3.4  .5 = 4! .5
